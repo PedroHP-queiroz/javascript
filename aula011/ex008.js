cc239547525b2b0bd5a772708c0416a3b71e9f07
pedro <pedropqueiroz1333@gmail.com>
2022-05-18 17:23:53 -0300

    new

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){ //cpndição simples
+    console.log(`Você ultrapassou a velocidade permitida e foi multado!`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
