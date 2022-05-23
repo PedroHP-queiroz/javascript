709e563d8285c52a329624aa66ec86a29cc4e586
pedro <pedropqueiroz1333@gmail.com>
2022-05-23 20:01:02 -0300

    new

--- aula012/ex011.js
+++ aula012/ex011.js
@@ -1,6 +1,11 @@
-var idade = 12
-if(idade < 18){
-    console.log('Menor de idade')
+var idade = 67
+console.log(`Voce tem ${idade} anos`)
+if(idade < 16) {
+    console.log('Não vota')
+} else if(idade < 18 || idade >65){
+        console.log('voto opcional')
 } else {
-    console.log('Maior de idade')
+    console.log('Voto obrigatorio')
 }
+    
+        
